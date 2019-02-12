19b20055397e5078e5f9de1430bc13953d9a287c
Harsh200 <harsh.saxena_ccv17@gla.ac.in>
2019-02-12 11:44:37 +0530

    comparison operator

--- Operator.py
+++ Operator.py
@@ -10,3 +10,20 @@ print(y/x)
 print(y//x)
 print(x//y)
 #above are arithmetic operator
+
+print(x>y)
+print(x<=y)
+print(x!=y)
+# above are comparison operator
+x=True
+y=False
+print(x and y)
+print(x or y)
+print(not x)
+print(not y)
+a=20
+b="harsh"
+print(a and b)
+print(a or b)
+a=3333333
+print(a and b)
