482f16b3969a470a00803750b95db3a9b705537f
Harsh200 <harsh.saxena_ccv17@gla.ac.in>
2019-02-12 11:53:03 +0530

    if else

---
+++ ifelse.py
@@ -0,0 +1,5 @@
+n=-1
+if n>0:
+    print(n,"is positive")
+else:
+    print(n,"is negative")
---
+++ vote.py
@@ -0,0 +1,5 @@
+age=int(input("Enter your age"))
+if age>=18:
+    print("congratulation! you are eligible for vote")
+else:
+    print("sorry ! you are not eligib22le for vote")
