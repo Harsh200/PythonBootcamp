5c51a5eeaca9f636d3d5b34a4401e6571b2c4bb2
Harsh200 <harsh.saxena_ccv17@gla.ac.in>
2019-02-12 10:10:13 +0530

    this is a basic syntax of python program

---
+++ Basicsyntax.py
@@ -0,0 +1,4 @@
+
+print("Hello this is python ")
+print("Hello World")
+print("Hello I am using python")
--- hello.py
+++ hello.py
@@ -1,2 +1,3 @@
 print("Hello World")
 print("Hello I am using python")
+print("Hello this is python ")
