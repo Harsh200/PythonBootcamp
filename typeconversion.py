745a8673100a9b346e07a6ec35433e610d78d88a
Harsh200 <harsh.saxena_ccv17@gla.ac.in>
2019-02-12 11:34:02 +0530

    this is type casting in python

---
+++ typeconversion.py
@@ -0,0 +1,18 @@
+a=101
+b=20.102
+c="Hello"
+print(type(a))
+print(type(b))
+print(type(c))
+d=a+b
+print(type(d))
+#Above are example of implicit type convention
+aa="123"
+bb="harsh"
+cc=aa+bb
+print(type(cc))
+print(type(aa))
+cd=int(aa)
+
+print(type(cd))
+
