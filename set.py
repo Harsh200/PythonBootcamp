805ff332a6bb21103b91b1cd1d756a6d1d614b33
Harsh200 <harsh.saxena_ccv17@gla.ac.in>
2019-02-19 23:22:22 +0530

    set program practice

---
+++ set.py
@@ -0,0 +1,14 @@
+set={1,2,3}
+print(set)
+set={1,222,3.0,5,4}
+print (set)
+print(set.add(1))
+set.update([2,3,777])
+print(set)
+set.remove(3)
+print(set)
+set.add(1)
+print(set)
+a={1,2,3,4,5,6}
+b={"harsh",222,4,5,777}
+print("union is",a|b)
