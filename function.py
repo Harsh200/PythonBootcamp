48b12973192fb9e8203ab08e126b1f5245ef0755
Harsh200 <harsh.saxena_ccv17@gla.ac.in>
2019-02-20 21:01:21 +0530

    new function fun

--- function.py
+++ function.py
@@ -4,4 +4,13 @@ def name(name):
 
 
 name("I am harsh kumar Saxena")
+# example of function byte
+def fun(list):
+    print("values inside ",list)
+    list[2]=50
+    print("values after in the list",list)
+    return
 
+
+list1 = [1,2,3]
+fun(list1)
