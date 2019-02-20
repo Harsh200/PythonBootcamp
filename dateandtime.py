9d8e3148f9206bafdc2c719e7d3d7af522ffc958
Harsh200 <harsh.saxena_ccv17@gla.ac.in>
2019-02-20 21:27:33 +0530

    we print month of calenadar

--- dateandtime.py
+++ dateandtime.py
@@ -1,6 +1,8 @@
 import time
 import calendar
 print(time.localtime())
-
 localtime=time.localtime(time.time())
 print("current time is",localtime)
+cal=calendar.month(2018,9)
+print("9th month is")
+print(cal)
