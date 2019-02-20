67e8e63056210118ff7b0f72cee6a35fa416b8d2
Harsh200 <harsh.saxena_ccv17@gla.ac.in>
2019-02-20 21:13:03 +0530

    finally keyword used

--- exceptionhandling.py
+++ exceptionhandling.py
@@ -1,8 +1,11 @@
 try:
     f=open("elif.py","w")
     f.write("#this file is used in as an example of exception handling")
-except IOError:
- print("file cannot found")
-else:
- print("content uploaded successfull")
-f.close()
+# except IOError:
+#  print("file cannot found")
+# else:
+#  print("content uploaded successfull")
+# f.close()
+finally:
+    print("error cannot find in data")
+    f.close()
