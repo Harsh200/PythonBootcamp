e1e4a4e65a7559a05f3d9bedebc1cd6bcc4f3179
Harsh200 <harsh.saxena_ccv17@gla.ac.in>
2019-02-23 11:37:42 +0530

    changes occur

--- computerusingencapsulation.py
+++ computerusingencapsulation.py
@@ -3,8 +3,7 @@ class computer:
      self._maxprice=1000
 
  def sell(self):
-         print("selling price",format(self._maxprice))
-
+         print("selling price()",format(self._maxprice))
  def setmaxprice(self,price):
          self._maxprice=price
 
