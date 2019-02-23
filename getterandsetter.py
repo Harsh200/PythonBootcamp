63998f6f983d6abb945315f0e6345a18c09ea741
Harsh200 <harsh.saxena_ccv17@gla.ac.in>
2019-02-23 11:49:41 +0530

    some changes is done due to error

--- getterandsetter.py
+++ getterandsetter.py
@@ -1,10 +1,16 @@
 class laptop(object):
     def __init__(self):
-        self.version=2018
+        self._version=2018
+
     def getversion(self):
-        print(self.version)
+        print(self._version)
+
     def setversion(self,version):
-        set.version=version
+        self._version = version
 
-obj=laptop()
 
+obj = laptop()
+obj.getversion()
+obj.setversion(2019)
+obj.getversion()
+print(obj._version)
