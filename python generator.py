63ee89134e566a0627170b4b7c9b1c2ba52c10c2
Harsh200 <harsh.saxena_ccv17@gla.ac.in>
2019-02-24 10:47:55 +0530

    correcting indentation

--- python generator.py
+++ python generator.py
@@ -1,8 +1,12 @@
 def gen():
     n=1
- print("this is printed first")
- yield n
+    print("this is printed first")
+    yield n
 
- n+=1
- print("this is printed second")
- yield n
+    n+=1
+    print("this is printed second")
+    yield n
+
+    n+=1
+    print("this is printed last")
+    yield n
